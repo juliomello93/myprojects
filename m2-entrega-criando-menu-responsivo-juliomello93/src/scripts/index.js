a6cceb887e754e1f05ea021a85dc53997e5ae38b
Julio Mello <51210110+juliomello93@users.noreply.github.com>
2022-11-24 14:51:58 -0300

    Add files via upload

---
+++ m2-entrega-criando-menu-responsivo-juliomello93/src/scripts/index.js
@@ -0,0 +1,8 @@
+let btnMenu = document.getElementById("btnMenu")
+let menuNav = document.querySelector(".menuNav")
+
+
+btnMenu.addEventListener("click", () => {    
+    menuNav.classList.toggle("showModal")
+    
+})
